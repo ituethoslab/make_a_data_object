258e7e225a64d7b2fa5dc24db6477dfe36b4436c
Mace Ojala <mace@kirjakaapeli.lib.hel.fi>
2018-09-11 20:57:49 +0200

    Cleaned the routes with input from pylint and additionally also from flake8

--- make_a_data_object/routes.py
+++ make_a_data_object/routes.py
@@ -1,4 +1,7 @@
-from flask import Flask, Response, render_template, request
+"""
+Routes for MVC style traffic control in Flask.
+"""
+from flask import Response, render_template, request
 from make_a_data_object import app
 from make_a_data_object.models import AdditiveDataObject, DefaultParameters
 
@@ -12,26 +15,29 @@ def hello_world():
 @app.route('/')
 def index():
     """Index route."""
-    return render_template('index.html', default_smoothing=DefaultParameters.smoothing)
+    return render_template('index.html',
+                           default_smoothing=DefaultParameters.smoothing)
 
 
 @app.route('/make', methods=['POST'])
 def submit():
-    a = request.form['abstract']
-    p = [float(p) for p in request.form['precipitation'].split(',')]
-    d = float(request.form['daylength'].replace(':', '.'))
+    """Route for receiving the input form."""
+    abstract = request.form['abstract']
+    precip = [float(p) for p in request.form['precipitation'].split(',')]
+    daylength = float(request.form['daylength'].replace(':', '.'))
     try:
-        s = int(request.form.get('smoothing'))
+        smoothing = int(request.form.get('smoothing'))
     except ValueError:
-        s = DefaultParameters.smoothing
+        smoothing = DefaultParameters.smoothing
 
     try:
         limit = int(request.form.get('limit'))
     except ValueError:
         limit = DefaultParameters.limit
 
-    f = request.form.get('filename') or DefaultParameters.filename
+    filename = request.form.get('filename') or DefaultParameters.filename
 
-    app.logger.debug("Parsed input a:{}, p:{}, d: {}, s:{}, l:{}, f:{}".format(a, p, d, s, limit, f))
-    do = AdditiveDataObject(a, p, d, size=450, limit=limit, alpha=s)
-    return Response(str(do), mimetype='text/plain', headers={"content-disposition": "attachment;filename={}".format(f)})
+    app.logger.debug("Parsed input abstract:{}, precip:{}, daylength: {}, smoothing:{}, limit:{}, filename:{}".format(abstract, precip, daylength, smoothing, limit, filename))
+    data_object = AdditiveDataObject(abstract, precip, daylength,
+                                     size=450, limit=limit, alpha=smoothing)
+    return Response(str(data_object), mimetype='text/plain', headers={"content-disposition": "attachment;filename={}".format(filename)})
